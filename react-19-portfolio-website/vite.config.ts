8a67866ef61fb42b076779247d6e45eb66a6a98a
Zeki Ahmed <changezworld3@gmail.com>
2026-09-12 06:40:26 +0300

    setup is done

--- react-19-portfolio-website/vite.config.ts
+++ react-19-portfolio-website/vite.config.ts
@@ -16,5 +16,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-  base: '/react-19-portfolio-website/', // Tells Vite to prepend this path automatically
+  // base: '/react-19-portfolio-website/', // Tells Vite to prepend this path automatically
 });
