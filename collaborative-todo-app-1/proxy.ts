6548b9b098b35215949b8a40c8e2ce64497d7922
Zeki Ahmed <changezworld3@gmail.com>
2026-09-15 02:52:15 +0300

    fix-remove-root-from-protected-routes-to-prevent-redirect-loop

--- collaborative-todo-app-1/proxy.ts
+++ collaborative-todo-app-1/proxy.ts
@@ -25,7 +25,7 @@ import { getSessionCookie } from 'better-auth/cookies'
  * Routes that require authentication.
  * Users without a session cookie will be redirected to /sign-in.
  */
-const protectedRoutes = ['/', '/boards']
+const protectedRoutes = ['/boards']
 
 /**
  * Routes that are only for unauthenticated users.
